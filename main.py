3cc34f2fcb2da787e16465793fd95e98eec51151
Taylor_Desktop <pythontaylortest@gmail.com>
2026-04-17 22:06:17 -0700

    Adds basic_cleaning code and successfully creates artifact

--- main.py
+++ main.py
@@ -53,7 +53,19 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                "src/basic_cleaning",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "basic_cleaned.csv",
+                    "output_type": "cleaned_data",
+                    "output_description": "Cleaned dataset artifact",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                }
+            )
 
         if "data_check" in active_steps:
             ##################
